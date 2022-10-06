dbfff41073fb2e14fce25ad0c8f115ac12ef0630
EliaSchiliro <eliabali372@gmail.com>
2022-10-06 20:20:51 +0200

    tttt

---
+++ main.py
@@ -0,0 +1,26 @@
+# a = 'Hello World'
+# print(type(a))
+# b = 10
+# print(type(b))
+# c = 100
+# ergebnis = b + c
+# print(type(ergebnis))
+# groesser = b > c or c < 1000
+# print(type(groesser))
+# d = 10.5
+# print(type(d)) 
+
+temp = 14
+
+if temp < 10:
+    print('Heizung an')
+elif temp < 15 and temp > 10:
+    print('Heizung 1 Stunde an')
+else:
+    print('Heizung aus')
+# for schleife 
+# für jede grad zahl ein * ausgibst
+print("*")
+print("*")
+print("*")
+
