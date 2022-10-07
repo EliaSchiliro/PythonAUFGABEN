a2b473a9c5808a620f80ed24911875de9fefe95a
E534550 <david.beisert@sbb.ch>
2022-10-08 01:05:32 +0200

    thermomemeter aufgabe

--- thermometer.py
+++ thermometer.py
@@ -1,1 +1,41 @@
 temp = 20
+
+""" 
+erzeuge eine ascii grafik wie unten abgebildet die anhand der variable temp dynamisch ein
+thermometer abbildet. Das Thermometer kann temparaturen bis 30 grad abbilden. Die temperaturen Zahlen neben dem Thermometer werden
+nur in 5 Schritten abgebildet
+
+   (^)
+30 | |
+   | |
+   | |
+   | |
+   | |
+25 | |
+   | |
+   | |
+   | |
+   | |
+20 |*|
+   |*|
+   |*|
+   |*|
+   |*|
+15 |*|
+   |*|
+   |*|
+   |*|
+   |*|
+10 |*|
+   |*|
+   |*|
+   |*|
+   |*|
+ 5 |*|
+   |*|
+   |*|
+   |*|
+   |*|
+ 0 |*|
+   (_)
+ """
