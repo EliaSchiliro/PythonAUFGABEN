4a93ee09b33ccefbee1c213c98fcb4316caf94f9
E534550 <david.beisert@sbb.ch>
2022-10-09 20:07:50 +0200

    square

---
+++ square.py
@@ -0,0 +1,31 @@
+
+
+def zeichne_quadrat(laenge):
+    print("Mache deine Hausaufgaben")
+
+# bsp drucke ein zeichen 10mal in der gleichen Zeile
+print("+" * 10)
+
+
+zeichne_quadrat(10)
+
+"""
+++++++++++
++        +
++        +
++        +
++        +
++        +
++        +
++        +
++        +
++        +
+++++++++++
+
+#
+##
+# #
+#  #
+#####
+
+"""
